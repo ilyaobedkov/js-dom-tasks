a796328e8bda5304a9c7278982609ab84edc9a54
ilyaobedkov <ilya.obedkovv@yandex.ru>
2026-06-02 17:07:52 +0300

    Update 7-element-manipulating.js

--- solutions/7-element-manipulating.js
+++ solutions/7-element-manipulating.js
@@ -1,5 +1,23 @@
 import camelCase from 'lodash/camelCase';
-
-// BEGIN
-
-// END
+const normalize = (document) => {
+  // Получаем все элементы на странице
+  const allElements = document.body.getElementsByTagName('*');
+  // Проходим по каждому элементу
+  for (const element of allElements) {
+    // Получаем список классов элемента
+    const classList = element.classList;
+    // Сохраняем текущие классы в массив, так как мы будем изменять classList
+    const currentClasses = Array.from(classList);
+    // Проходим по каждому классу элемента
+    for (const className of currentClasses) {
+      // Преобразуем имя класса в camelCase с помощью lodash
+      const camelCaseName = camelCase(className);
+      // Если имя класса изменилось
+      if (className !== camelCaseName) {
+        // Заменяем старый класс на новый
+        classList.replace(className, camelCaseName);
+      }
+    }
+  }
+};
+export default normalize;
